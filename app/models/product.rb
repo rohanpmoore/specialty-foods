afba64ed8ffb5ca738b11ae524ade0fb2ef24368
Rohan Moore <rohanpmoore@gmail.com>
2018-12-14 13:42:31 -0800

    add average rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,17 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :price, :numericality => true
   validates :origin, :presence => true
+
+  def average_rating
+    reviews = self.reviews
+    if reviews.length > 0
+      total = 0
+      reviews.each do |review|
+        total += review.rating
+      end
+      return (total.to_f/(reviews.length).to_f).round(1)
+    else
+      return "no reviews yet!"
+    end
+  end
 end
