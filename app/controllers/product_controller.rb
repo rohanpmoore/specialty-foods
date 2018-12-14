bac2f08775a6a8890b7d983224b8129a6de10aa5
Rohan Moore <rohanpmoore@gmail.com>
2018-12-14 10:55:24 -0800

    add product creation functionality

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,20 @@
+class ProductController < ApplicationController
+  def index
+    @products = Product.all
+  end
+  def new
+    @product = Product.new
+  end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+private
+  def product_params
+    params.require(:product).permit(:name, :price, :origin)
+  end
+end
