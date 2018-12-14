e698aa906fd661596d5c843bf533672b48d63ed8
Rohan Moore <rohanpmoore@gmail.com>
2018-12-14 11:28:39 -0800

    add all CRUD functionality to products

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,9 +2,15 @@ class ProductController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
+
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -13,6 +19,26 @@ class ProductController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(products_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
 private
   def product_params
     params.require(:product).permit(:name, :price, :origin)
